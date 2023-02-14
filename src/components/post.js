e749103ff615aa7d9118234805da60ecb7492c83
rhysw97 <rhysw97@gmail.com>
2023-02-14 12:17:52 +0000

    created social media app

---
+++ src/components/post.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Post() {
+    function postClick(e) {
+        document.getElementById
+    }
+    return(
+        <div>
+            <input type="text" placeholder="What is on your mind" />
+            <p onclick={postClick}>Post</p>
+        </div>
+    )
+}
