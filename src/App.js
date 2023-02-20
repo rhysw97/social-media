ff19868ee3b6e6419777bda6f2af5c889f9e91b3
rhysw97 <rhysw97@gmail.com>
2023-02-20 11:07:02 +0000

    updated app component to show register

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import CreatePost from './components/create-post'
+import CreatePost from './components/post-system/create-post'
 import Login from './components/login';
+import Register from './components/register';
 
 //allow user to post
 function App() {
   return (
     <div className="App">
-      <Login/>
+      <Register/>
     </div>
   );
 }
