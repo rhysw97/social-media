eb70f01e4f783afe93debcb18f380606ae46a09c
rhysw97 <62967056+rhysw97@users.noreply.github.com>
2023-03-02 13:58:35 +0000

    updated to show if email and age are valid

--- src/components/register/register.js
+++ src/components/register/register.js
@@ -10,10 +10,27 @@ import Password from "./password.js";
 export default function Register() {
     const usernameRef = useRef();
     const emailRef = useRef();
-    const dateOfBirthRef = useRef();
+    const [dateOfBirth, setDateOfBirth] = useState();
     const [userPassword, setUserPassword] = useState();
     const [currentIsValid, setCurrentIsValid] = useState();
+    const [ageMessage, setAgeMessage]  = useState();
+    const [emailMessage, setEmailMessage] = useState()
+    let validAge = false;
+    let emailValid = false;
+    const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
 
+    function findAge(dob) {
+        const currentDate = new Date()
+        const birthDate = new Date(dob)
+        let age = currentDate.getFullYear() - birthDate.getFullYear();
+        const month = currentDate.getMonth() - birthDate.getDate();
+        console.log(typeof age)
+        if(month < 0 || (month === 0 && currentDate.getDate() < birthDate.getDate)) {
+            age--
+        }
+        return age;
+    }
+    
     const validation = {
         email: false,
         dateOfBirth: false,
@@ -21,16 +38,35 @@ export default function Register() {
         passwordsMatch: false
     }
 
+    const handleEmail = event => {
+        emailValid = (emailRegex.test(event.target.value))
+        if(emailValid) {
+            setEmailMessage(null)
+        } else {
+            setEmailMessage(<p>Not a valid email</p>)
+        }
+    }
+
+    const handleDateOfBirth = (event) => {
+        setDateOfBirth(event.target.value)
+        const userAge = findAge(event.target.value);
+        if(userAge >= 18) {
+            validAge = true;
+            setAgeMessage(null)
+        } else {
+            validAge = false;
+            setAgeMessage(<p>You must be 18 or over to register</p>)
+        }
+    }
+
     const handleSubmit = (event) => {
         event.preventDefault();
         
-        
-
-        if(currentIsValid && Object.values(validation).every(value => value)) {
+        if(currentIsValid && validAge && emailValid) {
             const data = { 
                 username: usernameRef.target.value, 
                 email: usernameRef.target.value,
-                dateOfBirth: dateOfBirthRef.target.value,
+                dateOfBirth: dateOfBirth,
                 password: userPassword
             };
             postRequest('register', data)
@@ -55,16 +91,18 @@ export default function Register() {
                         type="text"
                         ref={emailRef}
                     />
+                    {emailMessage}
                 </div>
                 <div>
-                    <label htmlFor="dob">Email</label>
+                    <label htmlFor="dob">Date Of Birth</label>
                     <input
                         id="dob"
                         type="date"
-                        ref={dateOfBirthRef}
+                        onChange={handleDateOfBirth}
                     />
+                    {ageMessage}
                 </div>
-                <Password passwordState={setUserPassword} isValidState={setCurrentIsValid}/>
+                <Password setPasswordState={setUserPassword} setIsPasswordValid={setCurrentIsValid}/>
                 <button type="submit">Submit</button>
             </form>
        </div>
