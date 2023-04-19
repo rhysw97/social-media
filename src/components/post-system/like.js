bd16907d74979ba4a09e2946c9481767c27137bf
rhysw97 <rhysw97@gmail.com>
2023-04-19 14:22:05 +0100

    added state

--- src/components/post-system/like.js
+++ src/components/post-system/like.js
@@ -4,11 +4,13 @@ import {useState} from "react";
 
 
 export default function Post(props) {
-    
+    const [likedby, setLikedBy] = useState([])
+    //if user likes name gets added to array. cannot like again
+    //likes is length of array
     const handleLikes = () => {
    
         setLikes(currentLikes => currentLikes + 1)
-        
+
     }
     const [likes, setLikes] = useState(props.likes)
     return(
