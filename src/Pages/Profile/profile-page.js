7928351c233fca64d45386b3707c9a540c8966f3
rhysw97 <rhysw97@gmail.com>
2023-05-12 10:36:01 +0100

    updated profile page

--- src/Pages/Profile/profile-page.js
+++ src/Pages/Profile/profile-page.js
@@ -2,13 +2,21 @@ import { useEffect, useState } from "react"
 import { getRequest } from "../../utils/server-queries.ts"
 
 export default function ProfilePage(){
+    const [profile, setProfile] = useState({})
     useEffect(async () => {
-        
+        const response = await getRequest('users/profile')
+        setProfile(() => response)
     })
     
     return(
         <div>
-            <h1>Profile Page</h1>
+            <h1>{profile.username}</h1>
+            <img src={profile.profilePicture} alt="profile picture"/>
+            <p>{profile.bio}</p>
+            <ol>
+                {profile.genres.map(genre => <li><p>{genre}</p></li>)}
+            </ol>
+            
         </div>
     )
 }
