5f33a1d6612aee7a2197344ce1b1aa1b834a1648
rhysw97 <rhysw97@gmail.com>
2023-07-08 11:54:17 +0100

    updated profile page

--- src/Pages/Profile/profile-page.js
+++ src/Pages/Profile/profile-page.js
@@ -8,7 +8,7 @@ export default function ProfilePage(){
     },[])
 
     async function getProfile() { 
-        const response = await getRequest('/profile/get-profile')
+        const response = await getRequest('profile/get-profile')
         console.log('response', response)
         setProfile(()=> response)
     }
@@ -16,13 +16,17 @@ export default function ProfilePage(){
         return(<div>loading</div>)
     } else {
    return(
-        <div>
-            <h1>{profile.username}</h1>
-            <img src={`localhost:8000/users/${profile.profilePicture}`} alt="profile picture"/>
-            <p>{profile.bio}</p>
-            <ol>
-                {profile.genres.map((genre, index) => <li key={index}><p>{genre}</p></li>)}
-            </ol>
+        <div className=" flex flex-col content-center items-center">
+            <div className="ml-16 w-2/4">
+                <h1>You {profile.username}</h1>
+                <img src={`http://localhost:5000/uploads/${profile.profilePicture}`} alt="profile picture"/>
+                <p>BIO{profile.bio}</p>
+                <ol>
+                {
+                //{profile.genres.map((genre, index) => <li key={index}><p>{genre}</p></li>)}
+                }
+                </ol>
+            </div>
         </div>
         )
     }
