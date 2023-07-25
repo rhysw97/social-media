559b96cd8cb766c99c34a5b4ebed1d410d57fa9e
rhysw97 <rhysw97@gmail.com>
2023-07-25 14:20:20 +0100

    remved strict mode

--- src/App.js
+++ src/App.js
@@ -1,43 +1,38 @@
-
 import CreatePost from './Pages/post-page/create-post';
 import Register from './Pages/register/register';
 import LandingPage from './Pages/landing-page/landing-page';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import React, { useState, useMemo } from 'react';
+import React, { useState } from 'react';
 import Navbar from './components/UI/navbar/navbar';
 import "./assets/main.css"
 import { USERNAME, LOGGEDIN } from './data/contexts';
 import EditProfile from './Pages/edit-profile/edit-profile';
 import ProfilePage from './Pages/Profile/profile-page';
 
-
-
-
 //allow user to post
 function App() {
   const [usernameContext, setUsernameContext] = useState('')
   const [loggedInContext, setLoggedInContext] = useState(false)
 
+  //routing to allow page like structure to app
   return (
     <BrowserRouter>
-      <React.StrictMode>
-        <USERNAME.Provider value={{usernameContext, setUsernameContext}}>
-        <LOGGEDIN.Provider value={{loggedInContext, setLoggedInContext}}>
-          <div className="App">
-            <Routes>
-                <Route path="/" element={<LandingPage/>} />
-                <Route path="/register" element={<Register/>} />
-                <Route path="/post" element={<CreatePost/>} />
-                <Route path="/profile" element={<ProfilePage/>} />
-                <Route path="/edit-profile" element={<EditProfile/>} />
-            </Routes>
-            <Navbar/>
-          </div>
-        </LOGGEDIN.Provider>
-        </USERNAME.Provider>
-        </React.StrictMode>
-      </BrowserRouter>
-    
+      <USERNAME.Provider value={{usernameContext, setUsernameContext}}>
+      <LOGGEDIN.Provider value={{loggedInContext, setLoggedInContext}}>
+        <div className="App">
+          <Routes>
+              <Route path="/" element={<LandingPage/>} />
+              <Route path="/register" element={<Register/>} />
+              <Route path="/post" element={<CreatePost/>} />
+              <Route path="/profile" element={<ProfilePage/>} />
+              <Route path="/edit-profile" element={<EditProfile/>} />
+          </Routes>
+          <Navbar/>
+        </div>
+      </LOGGEDIN.Provider>
+      </USERNAME.Provider>
+    </BrowserRouter>
+  
   );
 }
 
