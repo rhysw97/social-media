e64af8a23e1f8d366d1dcc695a3330f7b4ecd31c
rhysw97 <rhysw97@gmail.com>
2023-08-10 09:33:43 +0100

    added images to comments

--- src/components/comments/postComment.js
+++ src/components/comments/postComment.js
@@ -4,11 +4,11 @@ import {getRequest, postRequest} from '../../utils/server-queries.ts'
 
 export default function PostComment(props) {
     const [comments, setComments] = useState([])
-    const [pictures, setPictures] = useState({})
     const commentInputRef = useRef()
    
     useEffect( () => {
         getComments()
+    
     }, [])
 
     async function getComments() { 
@@ -17,16 +17,9 @@ export default function PostComment(props) {
         console.log('response', response.comments)
         
         response.comments.forEach(comment => {
-            {console.log('added')}
-            setComments(currentComments => [...currentComments, {
-                id:comment.id,
-                username: comment.user,
-                message: comment.message,
-                likes: comment.likes,
-            }])
+            setComments(currentComments => [...currentComments, comment])
         });
-        displayComments()
-        console.log('viuewcomments', comments)
+        
     }
 
     const commentOnPost = async (e) => {
@@ -47,30 +40,19 @@ export default function PostComment(props) {
         }
     }
 
-    async function displayComments() {
-        const tempPictures = {}
-        for(let i = 0; i < comments.length; i++) {
-            const username = comments[i].username
-            const response = await getRequest(`profile/profile-pic?username=${username}`)
-            console.log('pictures', response)
-            tempPictures[username] = response.data
-        }  
-
-        setPictures(() => tempPictures)
-    }
-    
+   
     if(comments) {
         return (
-            <div>
-                <div className="comments">
-                    {comments.map(async (comment, index) => {
-                        return <div key={index}>
-                           <div>
-                         
-                                <h2>{comment.username}</h2>
+            <div className="h-full">
+                <div className="comments h-96 overflow-scroll">
+                    {comments.map((comment, index) => {
+                        return <div className="" key={index}>
+                           <div className="">
+                                <img className="w-20 h-20" src={`./uploads/${comment.profilePicture}`}/>
+                                <h2>{comment.user}</h2>
                            </div>
                                 <p>{comment.message}</p>
-                           </div>
+                        </div>
                     })}
                 </div>
                 <input placeholder="add comment" ref={commentInputRef}/>
