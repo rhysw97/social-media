1e873333a0904d13634a40cb2cdcb174afe451fd
rhysw97 <rhysw97@gmail.com>
2023-12-20 14:53:33 +0000

    update events

--- src/Pages/events/events.js
+++ src/Pages/events/events.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function events() {
+
+    const [events, setEvents] = useState([])
+
+    useEffect(() => {
+        setEvents()
+    }, [])
     return (
         <div>
             <h1 className="header">Events</h1>
 
-            <
+            <div>
+
+            </div>
         </div>
     );
 }
