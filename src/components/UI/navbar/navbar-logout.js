b83a8a5483a1864e950fb279f8689dc0b753f84a
rhysw97 <62967056+rhysw97@users.noreply.github.com>
2024-01-06 12:22:56 +0200

    updated

---
+++ src/components/UI/navbar/navbar-logout.js
@@ -0,0 +1,19 @@
+import { useNavigate } from "react-router-dom"
+
+export default function NavbarIcon({icon, text='tooltip', path='/'}) {
+    const navigate = useNavigate()
+
+    const navigateToPage = () => {
+        console.log('to the profile page')
+        navigate(path)
+    }
+    return (
+        <div onClick={navigateToPage} className="navbar-icon group">
+            {icon}
+
+            <span className="navbar-tooltip group-hover:scale-100">
+                {text}
+            </span>
+        </div>
+    )
+}
