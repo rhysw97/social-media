a0dc46fe689ba2e76ea89d7c708c296ad3ed3d07
rhysw97 <rhysw97@gmail.com>
2024-01-07 14:02:22 +0000

    updated to allow user to create event and send data to backend

--- src/Pages/events/create-event.js
+++ src/Pages/events/create-event.js
@@ -1,18 +1,43 @@
-import React from "react";
+import React, { useRef } from "react";
+import { postRequest } from "../../utils/server-queries.ts";
 
 export default function CreateEvent() {
-
+    const artistInputRef = useRef(null)
+    const genreInputRef = useRef(null)
+    const locationInputRef = useRef(null)
+    const imageInputRef = useRef(null)
+    const dateInputRef = useRef(null)
+    const timeInputRef = useRef(null)
     
+    const handleSubmit = () => {
+        const data = {
+            artist: artistInputRef.current.value,
+            genre: genreInputRef.current.value,
+            location: locationInputRef.current.value,
+            date: dateInputRef.current.value,
+            eventPicture: imageInputRef.current.value,
+            time: timeInputRef.current.value
+        }
+
+        postRequest('events', data)
+
+        artistInputRef.current.value = null
+        genreInputRef.current.value = null
+        locationInputRef.current.value = null
+        dateInputRef.current.value = null
+        timeInputRef.current.value = null
+    }
 
     return(
-        <form>
-            <input className="input-field" placeholder="Artist Name"required/>
-            <input className="input-field" placeholder="location"required/>
-            <input className="input-field" placeholder="Genre" required/>
-            <input className="input-field" placeholder="Genre" required/>
-            <input className="" type="time"required/>
+        <form className="flex flex-col items-center">
+            <input className="input-field" placeholder="Artist Name" ref={artistInputRef} required/>
+            <input className="input-field" placeholder="Genre" ref={genreInputRef} required/>
+            <input className="input-field" placeholder="Location" ref={locationInputRef} required/>
+            <input className="input-field" placeholder="Event Image Link" ref={imageInputRef} required/>
+            <input className="input-field" type="date" ref={dateInputRef} required/>
+            <input className="" type="time" ref={timeInputRef} required/>
 
-            <button type="submit">Create Event</button>
+            <button type="submit" onClick={handleSubmit}>Create Event</button>
         </form>
     )
 }
