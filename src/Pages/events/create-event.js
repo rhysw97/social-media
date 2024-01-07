2ecfbfa1539dc1309562d4c350ace2555a46a481
rhysw97 <rhysw97@gmail.com>
2024-01-07 13:29:28 +0000

    added functionality to create events

--- src/Pages/events/create-event.js
+++ src/Pages/events/create-event.js
@@ -2,12 +2,15 @@ import React from "react";
 
 export default function CreateEvent() {
 
+    
+
     return(
         <form>
-            <input className="input-field" placeholder="Artist Name"/>
-            <input className="input-field" placeholder="location"/>
-            <input className="input-field" placeholder="Genre" />
-            <input className="" type="time"/>
+            <input className="input-field" placeholder="Artist Name"required/>
+            <input className="input-field" placeholder="location"required/>
+            <input className="input-field" placeholder="Genre" required/>
+            <input className="input-field" placeholder="Genre" required/>
+            <input className="" type="time"required/>
 
             <button type="submit">Create Event</button>
         </form>
--- src/Pages/events/events.js
+++ src/Pages/events/events.js
@@ -1,10 +1,14 @@
 import React, { useEffect, useState } from "react";
-import   {BlackMassImage, HamishHawkImage, QuelleChrisImage } from "../../assets/images/image-links"  
-import { FaCalendar, FaClock, FaLocationArrow} from "react-icons/fa";                         
+import   {BlackMassImage, HamishHawkImage, QuelleChrisImage } from "./../../assets/images/image-links"  
+import { FaCalendar, FaClock, FaLocationArrow, FaPlus} from "react-icons/fa";
+import CreateEvent from './create-event'   
+import Modal from './../../components/UI/modal/modal';        
+
 
 export default function Events() {
 
     const [events, setEvents] = useState([])
+    const [createEventModalActive, setCreateEventModalActive] = useState(false)
 
     useEffect(() => {
         setEvents(() => {
@@ -44,24 +48,26 @@ export default function Events() {
             </header>
 
             <div className="ml-16 flex flex-row w-[100%] flex-wrap">
-                    <div className="flex flex-row gap-2 flex-wrap w-[100%]">
-                        {events.map(event => {
-                            return (<div className="basis-[45%] justify-around bg-green-500 p-4 rounded-lg mx-auto shadow-black shadow-md">
-                                <div className="flex flex-col">
-                                    <img className="w-[100%] h-[250px] object-cover" src={event.img }></img>
-                                    <h2 className="text-center text-shadow-lg py-5 font-extrabold text-4xl">{event.artist}</h2>
-                                    
-                                </div>
-                                <p className="text-center text-xl font-semibold">{event.location}</p>
-                                <div className="w-[100%] flex flex-row my-4">
-                                    <div className="flex flex-col items-center basis-1/2"><FaCalendar></FaCalendar><p>{event.date}</p></div>
-                                    <div className="flex flex-col items-center basis-1/2"><FaClock></FaClock><p>{event.time}</p></div>
-                                </div>
-                            </div>)
-                        })}
-
-                    </div>
+                <div className="flex flex-row gap-2 flex-wrap w-[100%]">
+                    {events.map(event => {
+                        return (<div className="basis-[45%] justify-around bg-green-500 p-4 rounded-lg mx-auto shadow-black shadow-md">
+                            <div className="flex flex-col">
+                                <img className="w-[100%] h-[250px] object-cover" src={event.img }></img>
+                                <h2 className="text-center text-shadow-lg py-5 font-extrabold text-4xl">{event.artist}</h2>
+                                
+                            </div>
+                            <p className="text-center text-xl font-semibold">{event.location}</p>
+                            <div className="w-[100%] flex flex-row my-4">
+                                <div className="flex flex-col items-center basis-1/2"><FaCalendar></FaCalendar><p>{event.date}</p></div>
+                                <div className="flex flex-col items-center basis-1/2"><FaClock></FaClock><p>{event.time}</p></div>
+                            </div>
+                        </div>)
+                    })}
+                    
                 </div>
             </div>
+            <div className="bg-green-700 py-4 px-10 rounded-full shadow-lg shadow-slate-700  fixed bottom-4 right-5 hover:shadow-none hover:bottom-3" onClick= { () => setCreateEventModalActive(true)} ><p>Create Event</p></div>
+            <Modal show={createEventModalActive} close={()=> setCreateEventModalActive(false)} content={<CreateEvent />} title={"Edit"}/>
+        </div>
     );
 }
